cbaf0c8a2929197f07f5a1167b6b78e5bbbae041
Walid Mostafa <wella4321@gmail.com>
2021-12-20 10:33:16 +0200

    .

--- nft-project/src/App.js
+++ nft-project/src/App.js
@@ -1,37 +1,41 @@
-import './App.css'
-import Header from './components/Header'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Punklist from './components/Punklist'
-import Main from './components/Main'
+import "./App.css";
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Punklist from "./components/Punklist";
+import Main from "./components/Main";
 
 function App() {
-  const [punkListData, setPunkListData] = useState([])
-  const [selectedPunk, setSelectedPunk] = useState(0)
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
 
   useEffect(() => {
     const getMyNfts = async () => {
-      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0xA621E0b815c79deEe3900D663E03d1192f7ede89&order_direction=asc')
-      console.log(openseaData.data.assets)
-      setPunkListData(openseaData.data.assets)
-    }
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xA621E0b815c79deEe3900D663E03d1192f7ede89&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
 
-    return getMyNfts()
-  }, [])
+    return getMyNfts();
+  }, []);
 
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
       {punkListData.length > 0 && (
         <>
           <Main punkListData={punkListData} selectedPunk={selectedPunk} />
-          <Punklist punkListData={punkListData}
-            setSelectedPunk={setSelectedPunk} />
+          <Punklist
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
+          />
           )
         </>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
